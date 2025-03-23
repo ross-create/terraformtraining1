1006721b8f9e1646703e4d8d4c2e3106103d2196
Ross Stevens <ross.stevens20@gmail.com>
2025-03-23 23:03:22 +0000

    first commit test

---
+++ main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_version = ">=1.3.0"
+  required_providers {
+    azurerm = {
+      "source" = "hashicorp/azurerm"
+      version  = "3.43.0"
+    }
+  }
+  cloud {
+
+    organization = "Ross-Training777"
+
+    workspaces {
+      name = "githubtest1"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = truegit
+}
+
+resource "random_string" "uniquestring" {
+  length  = 20
+  special = false
+  upper   = false
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "811-6a9073eb-provide-continuous-delivery-with-gith"
+  location = "westus"
+}
+
+resource "azurerm_storage_account" "storageaccount" {
+  name                     = "stg${random_string.uniquestring.result}"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
